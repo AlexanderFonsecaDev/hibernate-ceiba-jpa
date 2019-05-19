53ec59e6aea7bf239bc731b7daa7e16745e6cfbc
Dante <jhonalexanderfonseca1993@gmail.com>
2019-05-19 08:48:10 -0500

    paquetes creados

---
+++ src/main/java/com/ceiba/hibernate/vista/index.java
@@ -0,0 +1,15 @@
+package com.ceiba.hibernate.vista;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SpringLayout;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JScrollPane;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.JTable;
+
+public class index extends JFrame{
+
+}
